83b20b7bd3ea1b03169ab1ce3da365eae6c1595b
Allan <allanbrados@gmail.com>
2016-09-24 03:07:06 -0300

    Uploading my study from Particles102

---
+++ Assets/Particles 102/Scripts/ParticleRotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleRotate : MonoBehaviour {
+
+    public float angleSpeed;
+
+	void Update () {
+        transform.Rotate(Vector3.forward, angleSpeed * Time.deltaTime);
+	}
+}
